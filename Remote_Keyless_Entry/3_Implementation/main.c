10b8b21b89e4af3518e7c5341fb3d150793fa8f2
akashpenchalapadu9 <46950972+akashpenchalapadu9@users.noreply.github.com>
2022-03-12 10:19:13 +0530

    Add files via upload

---
+++ Remote_Keyless_Entry/3_Implementation/main.c
@@ -0,0 +1,238 @@
+#include "MyStm32f407xx.h"
+#include <stdint.h>
+
+#define BTN_PRESSED ENABLE
+
+void Lock(void);
+void UnLock(void);
+void clockwise(void);
+void anticlockwise(void);
+void pw(void);
+void pw_2(void);
+void pw_3(void);
+void pw_4(void);
+/**
+ * @brief This function deals with debouncing delay of blue switch
+ *
+ */
+void delay(void)
+{                                                                                          // PS NO = 99007659
+	for(uint32_t i=0; i<30000000; i++);
+}
+
+int main(void)
+{
+
+	GPIO_Handle_t GpioLed, GPIOBtn; 							//Initialising variables for led and blue switch
+	/**
+	 * @brief Setup for LEDs
+	 *
+	 */
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12 ;	//Green Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+ 	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;		//Orange Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;		//Red Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;		//Blue Led
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+	/**
+	 * @brief Setup for Blue Switch
+	 *
+	 */
+	GPIOBtn.pGPIOx = GPIOA;
+    GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&GPIOBtn);
+
+	while(1)                                                                                       
+	{                                                                                       // PS NO= 99007658
+		pw();				//encryption for Lock function
+
+		pw_2();				//encryption for UnLock function
+
+		pw_3();				//encryption for clockwise function
+
+		pw_4();				//encryption for anticlockwise function
+
+	}
+
+}
+/**
+ * @brief encryption for Lock function
+ *
+ */
+void pw(void)
+{
+	p1:if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+	{
+		Lock();
+	}
+	else
+		goto p1;
+
+}
+
+/**
+ * @brief encryption for UnLock function
+ *
+ */
+void pw_2(void)
+{
+	uint32_t i=0, count = 1;
+	for(i=0;i<2;i++)
+	{
+	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+		count++;
+	else
+		i=0;
+	}
+	if(count==2)
+		{
+
+			UnLock();
+
+		}
+}
+/**
+ * @brief encryption for clockwise function
+ *
+ */
+void pw_3(void)
+{                                                                                    // PS NO = 99007660
+	uint32_t i=0, count = 1;
+	for(i=0;i<3;i++)
+	{
+	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+		count++;
+	else
+		i=0;
+	}
+	if(count==3)
+		{
+
+			clockwise();
+
+		}
+}
+/**
+ * @brief encryption for anticlock function
+ *
+ */
+void pw_4(void)
+{
+	uint32_t i=0, count = 1;
+	for(i=0;i<4;i++)
+	{
+	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)== BTN_PRESSED)
+		count++;
+	else
+		i=0;
+	}
+	if(count==4)
+		{
+
+			anticlockwise();
+
+		}
+}
+
+
+
+
+/**
+ * @brief Lock function for locking door
+ *
+ */
+void Lock(void)
+{
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+}
+/**
+ * @brief UnLock function for unlocking door
+ *
+ */
+void UnLock(void)
+{
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+}
+/**
+ * @brief clockwise function for alarm
+ *
+ */
+void clockwise(void)
+{                                                                                     // PS NO = 99007661
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+}
+/**
+ * @brief anticlockwise function for approach light
+ *
+ */
+void anticlockwise(void)
+{
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
+	delay();
+	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
+}
